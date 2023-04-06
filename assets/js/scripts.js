1998adb8c978c30239d70524e219fb4789775d3e
HectorJosueDH <hectorjosue78@gmail.com>
2023-04-06 13:30:32 -0600

    Added JS functions, the quiz now runs when clicking the start button, added coments to the JS, next step is the sbumit initials and the high score table

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,38 +1,113 @@
-var question = document.querySelector('#question');
-var choices = document.querySelector('#choice-text');
-var scoreText = document.querySelector('#score');
+/* Start of the application when clicking on the 'Start Quiz' button */
+document.addEventListener("DOMContentLoaded", () => {
+  const indexEl =  document.querySelector("#start");
+  const createAccountForm = document.querySelector("#quiz");
 
-let currentQuestion = {};
-let acceptedAnswers = true;
-let score = 0;
-let questionCounter = 0;
-let availableQuestions = [];
+  document.querySelector("#start-quiz").addEventListener("click", e => {
+    e.preventDefault();
+    indexEl.classList.add("hidden");
+    createAccountForm.classList.remove("hidden");
 
-let questions = [
-    {
-        question: 'Commonly used data types DO not include:',
-        choices: ['strings', 'boolean', 'alerts', 'numbers'],
-        answer: 'alerts'
-    },
-    {
-        question: 'The condition in an if / else statement is enclosed with ________.',
-        choices: ['quotes', 'curly brackets', 'parenthesis', 'square brackets'],
-        answer: 'curly brackets'
-    },
-    {
-        question: 'A very useful tool used during development and debbugging for printing content to the debugger is:',
-        choices: ['JavaScript', 'terminal / Bash', 'for loops', 'console.log'],
-        answer: 'console.log'
-    },
-    {
-        question: 'Arrays in JavaScript can be used to store ________.',
-        choices: ['numebrs and strings', 'other arrays', 'booleans', 'all of the above'],
-        answer: 'all of the above'
-    },
-    {
-        question: 'String values must be enclosed within ________ when being assigned to variables.',
-        choices: ['commas', 'curly brackets', 'quotes', 'parenthesis'],
-        answer: 'quotes'
-    }
+    loadQuestion();
+    startTimer();
+  });
+});
+
+/* Array named quiz that stores objects that store the questions, choices and the correct answers for each question */
+const quiz = [
+  {
+    question: "Commonly used data types DO not include:",
+    choices: ["1. strings", "2. boolean", "3. alerts", "4. numbers"],
+    correctAnswer: "3. alerts"
+  },
+  {
+    question: "The condition in an if / else statement is enclosed with ________.",
+    choices: ["1. quotes", "2. curly brackets", "3. parenthesis", "4. square brackets"],
+    correctAnswer: "2. curly brackets"
+  },
+  {
+    question: "A very useful tool used during development and debbugging for printing content to the debugger is:",
+    choices: ["1. JavaScript", "2. terminal / Bash", "3. for loops", "4. console.log"],
+    correctAnswer: "4. console.log"
+  },
+  {
+    question: "Arrays in JavaScript can be used to store ________.",
+    choices: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
+    correctAnswer: "4. all of the above"
+  },
+  {
+    question: "String values must be enclosed within ________ when being assigned to variables.",
+    choices: ["1. commas", "2. curly brackets", "3. quotes", "4. parenthesis"],
+    correctAnswer: "3. quotes"
+  }
 ];
 
+/* Variables; arrayQuestion represent the position of the questions on que quiz array above, score stores how many correct, wrongs we had
+while reolving the quiz , the timer is the time we have to answer the quiz before ir closes, the timerId is how we represent the timer visually 
+and functionally */
+var arrayQuestion = 0;
+var score = 0;
+var timer = 76;
+var timerId = null;
+
+
+/* Function; loads questions and the choices, the 'for' creates the choices for each 'choices' on the quiz array, ex: if choices had 5 disctinct 
+values then 5 answer boxes will be created */
+function loadQuestion() {
+  const questionEl = document.getElementById("question");
+  const choicesEl = document.getElementById("choices");
+  const thisQuiz = quiz[arrayQuestion];
+
+  questionEl.textContent = thisQuiz.question;
+  choicesEl.innerHTML = "";
+
+  for (let i = 0; i < thisQuiz.choices.length; i++) {
+    const choice = thisQuiz.choices[i];
+    const button = document.createElement("button");
+    button.className = 'btn';
+    button.textContent = choice;
+    button.addEventListener("click", function() {
+      checkAnswer(choice);
+    });
+    choicesEl.appendChild(button);
+  }
+}
+
+// Function; check if the the right answer was selected and returns "Correct" else if no tthe right answer returns "Wrong"
+function checkAnswer(choice) {
+  const thisQuiz = quiz[arrayQuestion];
+  // If the answer is correct, add to the score and display a message
+
+  if (choice === thisQuiz.correctAnswer) {
+    score++;
+    document.getElementById("result").textContent = "Correct!";
+  } else {
+    document.getElementById("result").textContent = "Wrong!";
+  }
+
+  // Continues with the next question by incrementing the array quiz position to the next one
+  arrayQuestion++;
+  if (arrayQuestion < quiz.length) {
+    loadQuestion();
+  } else {
+    endQuiz();
+  }
+}
+
+// function; starts timer when clicking the start quiz button, also stops when the user completes the quiz or when the timer reaches 0
+function startTimer() {
+  const timerEl = document.getElementById("timer");
+  timerId = setInterval(function() {
+    timer--;
+    timerEl.textContent = "Time: " + timer;
+    if (timer <= 0) {
+      endQuiz();
+    }
+  }, 1000);
+}
+
+// function; ends the quiz and returs in screen how many points the user got, also stops the timer
+function endQuiz() {
+  clearInterval(timerId);
+  document.getElementById("quiz").innerHTML = "<h2>All done!</h2><p>Your final score is  " + score + ".";
+}
